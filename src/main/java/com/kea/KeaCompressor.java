c3ec259293cb2ace7f019eb00a58cf9ca118c03e
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2025-05-21 17:13:49 +0700

    KEA compression library with Gradle and example

---
+++ src/main/java/com/kea/KeaCompressor.java
@@ -0,0 +1,14 @@
+package com.kea;
+
+public class KeaCompressor {
+
+    public byte[] compress(byte[] input) {
+        // TODO: Реализовать алгоритм KEA
+        return input;
+    }
+
+    public byte[] decompress(byte[] input) {
+        // TODO: Реализовать алгоритм распаковки KEA
+        return input;
+    }
+}
